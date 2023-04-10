b891c68035de8f2d863082441d10a468a3e88a70
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-04-10 04:23:08 +0200

    doing task 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const FEEDBACK_FS = 'feedback-form-state';
+
+let userData = {};
+
+const feedbackForm = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+
+feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+feedbackForm.addEventListener('submit', onFormSubmit);
+
+fillInputs();
+
+function onFormInput(e) {
+  userData[e.target.name] = e.target.value;
+  localStorage.setItem(FEEDBACK_FS, JSON.stringify(userData));
+}
+
+function fillInputs() {
+  const savedData = JSON.parse(localStorage.getItem(FEEDBACK_FS));
+  if (savedData) {
+    input.value = savedData.email || '';
+    textarea.value = savedData.message || '';
+    userData = savedData;
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  userData = {};
+  localStorage.removeItem(FEEDBACK_FS);
+}
