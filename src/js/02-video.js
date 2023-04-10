a12e29557b2f6e0aa1ad24d82b7f6f406cf89a00
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-04-10 06:54:12 +0200

    add fixes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,18 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
 const VPL_CURRENT_TIME = 'videoplayer-current-time';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 const currentTime = JSON.parse(localStorage.getItem(VPL_CURRENT_TIME));
 
-player
-  .setCurrentTime(currentTime)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
-
-const onPlay = function (e) {
-  localStorage.setItem(VPL_CURRENT_TIME, JSON.stringify(e.seconds));
-};
+if (currentTime !== null) {
+  player.setCurrentTime(currentTime);
+}
 
 player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(e) {
+  localStorage.setItem(VPL_CURRENT_TIME, JSON.stringify(e.seconds));
+}
